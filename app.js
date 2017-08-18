e0675daf814457ec026e19181a18c15aecf2daf9
Tarun KumarTomar <tarun.kumartomar@in.abb.com>
2017-08-18 11:18:59 +0530

    add refresh rate regulator

--- app.js
+++ app.js
@@ -3,46 +3,57 @@ var fs = require('fs');
 var url = require('url');
 
 var index = fs.readFileSync('index.html');
-
-var server = http.createServer(function(req,res){
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+var server = http.createServer(function (req, res) {
     //console.log(req);
 
     let filename = url.parse(req.url).pathname;
-
-    if(filename==='/cpufirstdata'){
-         res.writeHead(200,{'ContentType':'application/json'});
-         let tmpdata = [{ 'time':'8/17/17 10:45:00','value':1}
-                        ,{ 'time':'8/17/17 11:45:00','value':2}
-                        ,{ 'time':'8/17/17 12:45:00','value':3}
-                        ,{ 'time':'8/18/17 4:45:00','value':4}
-                        ,{ 'time':'8/18/17 6:45:00','value':5}
-                        ,{ 'time':'8/18/17 7:45:00','value':6}
-                        ,{ 'time':'8/19/17 3:45:00','value':8}
-                        ,{ 'time':'8/19/17 7:45:00','value':12}
-                        ,{ 'time':'8/19/17 8:45:00','value':10}
-                        ,{ 'time':'8/19/17 9:45:00','value':11}
-                        ,{ 'time':'8/20/17 10:45:00','value':15}
-                        ,{ 'time':'8/20/17 11:45:00','value':13}
-                        ,{ 'time':'8/20/17 12:45:00','value':12}
-                        ,{ 'time':'8/20/17 12:46:00','value':10}
-                        ,{ 'time':'8/20/17 12:47:00','value':14}
-                        ,{ 'time':'8/21/17 10:45:00','value':12}
-                       ];
+    if (filename === '/telemetrydata') {
+        var today = new Date();
+        var date =  (today.getMonth() + 1) + '/' + today.getDate()+'/'+today.getFullYear();
+        var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds()+":"+today.getMilliseconds();
+        var dateTime = date + ' ' + time;
+        res.writeHead(200, { 'ContentType': 'application/json' });
+        let tmpdata = [{ 'time': dateTime, 'value': getRandomInt(1, 16) }];
          res.write(JSON.stringify(tmpdata));
-         res.end();
+        res.end();
+    }
+    else if (filename === '/cpufirstdata') {
+        res.writeHead(200, { 'ContentType': 'application/json' });
+        let tmpdata = [
+            { 'time': '8/17/2017 01:45:00', 'value': 2 }
+            , { 'time': '8/17/2017 2:45:00', 'value': 3 }
+            , { 'time': '8/17/2017 2:46:00', 'value': 4 }
+            , { 'time': '8/17/2017 2:47:00', 'value': 5 }
+            , { 'time': '8/17/2017 2:48:00', 'value': 6 }
+            , { 'time': '8/17/2017 2:49:00', 'value': 8 }
+            , { 'time': '8/17/2017 2:50:00', 'value': 12 }
+            , { 'time': '8/17/2017 2:51:00', 'value': 10 }
+            , { 'time': '8/17/2017 2:52:00', 'value': 11 }
+            , { 'time': '8/17/2017 2:53:00', 'value': 15 }
+            , { 'time': '8/17/2017 2:54:00', 'value': 13 }
+            , { 'time': '8/17/2017 2:55:00', 'value': 12 }
+            , { 'time': '8/17/2017 2:56:00', 'value': 10 }
+            , { 'time': '8/17/2017 2:57:00', 'value': 14 }
+            , { 'time': '8/17/2017 2:58:00', 'value': 12 }
+        ];
+        res.write(JSON.stringify(tmpdata));
+        res.end();
     }
 
-if(filename.length===1) filename = "/index.html";
-filename = __dirname+filename;
+    if (filename.length === 1) filename = "/index.html";
+    filename = __dirname + filename;
 
 
-    fs.readFile(filename,function(err,data){
+    fs.readFile(filename, function (err, data) {
 
-        if(err){
-            res.writeHead(404,{'ContentType':'text/plain'});
-            res.end("Page "+filename+" not found");
-        }else{
-            res.writeHead(200,{'ContentType':'text/html'});
+        if (err) {
+            res.writeHead(404, { 'ContentType': 'text/plain' });
+            res.end("Page " + filename + " not found");
+        } else {
+            res.writeHead(200, { 'ContentType': 'text/html' });
             res.end(data);
         }
 
--- timeseries.js
+++ timeseries.js
@@ -1,8 +1,24 @@
-var timeSeries = function (parentDiv) {
+var alldata = [];
+
+var timeSeries = function (options) {
+
+        // define default options if not explicitly defined
+        if(!(width in options))
+                options['width']=960;
+        if(!(height in options))
+                options['height']=500;
 
         // create an svg, with width and margin
+        var timeserieschart = {
+                options: {
+                        refreshRate: 1000,
+                        refresh: true
+                }
+        };
+        parentDiv = d3.select(options.divid);
+        var width = 900, height = 500, margin = { top: 20, right: 20, bottom: 30, left: 50 };
 
-        var width = 900, height = 500, margin = {top: 20, right: 20, bottom: 30, left: 50};
+
         var svg = parentDiv.append('svg').attr('width', width)
                 .attr('height', height)
                 .attr('style', 'background-color:black');
@@ -15,31 +31,32 @@ var timeSeries = function (parentDiv) {
                 .rangeRound([0, width]);
         var yscale = d3.scaleLinear()
                 .rangeRound([height, 0]);
-        var parseDate = d3.timeParse('%m/%d/%y %H:%M:%S')
+        var parseDate = d3.timeParse('%m/%d/%Y %H:%M:%S:%L')
         area = d3.area()
                 .x(function (d) { return xscale(parseDate(d.time)); })
                 .y1(function (d) { return yscale(d.value); });
 
-                var line = new d3.line()
+        var line = new d3.line()
                 .x(function (d) { return xscale(parseDate(d.time)); })
                 .y(function (d) { return yscale(d.value); });
-// gridlines in x axis function
-function make_x_gridlines() {		
-    return d3.axisBottom(xscale)
-        .ticks(5)
-}
+        // gridlines in x axis function
+        function make_x_gridlines() {
+                return d3.axisBottom(xscale)
+                        .ticks(5)
+        }
 
-// gridlines in y axis function
-function make_y_gridlines() {		
-    return d3.axisLeft(yscale)
-        .ticks(5)
-}
+        // gridlines in y axis function
+        function make_y_gridlines() {
+                return d3.axisLeft(yscale)
+                        .ticks(5)
+        }
         // get last 1 week data by default
         d3.json('/cpufirstdata', function (err, res) {
                 if (err) throw err;
-
+                alldata = res;
                 var maxTime = d3.max(res, function (d) { return parseDate(d.time); });
-                var minTime = d3.timeDay.offset(maxTime, -7);
+                var minTime = d3.min(res, function (d) { return parseDate(d.time); });
+                //var minTime = d3.timeDay.offset(maxTime, -7);
                 xscale.domain([minTime, maxTime]);
                 yscale.domain([0, d3.max(res, function (d) { return d.value; })]);
                 area.y0(yscale(0));
@@ -54,11 +71,11 @@ function make_y_gridlines() {
                 g.append('path')
                         .datum(res)
                         .attr('fill', '#00c109')
-                        .attr('class','indicator')
-                        .attr('stroke','#8dff05')
-                        .attr('stroke-width','2')
-                        .attr('shape-rendering','crispEdges')
-                        .attr('style','opacity:0.5')
+                        .attr('class', 'indicator')
+                        .attr('stroke', '#8dff05')
+                        .attr('stroke-width', '2')
+                        .attr('shape-rendering', 'crispEdges')
+                        .attr('style', 'opacity:0.5')
                         .attr('d', area);
 
                 // g.append('path')
@@ -66,7 +83,7 @@ function make_y_gridlines() {
                 //         .attr('d',line);
                 svg.append("g")
                         .attr("class", "grid")
-                        .attr("transform", "translate("+margin.left+"," + (height+margin.top) + ")")
+                        .attr("transform", "translate(" + margin.left + "," + (height + margin.top) + ")")
                         .call(make_x_gridlines()
                                 .tickSize(-height)
                                 .tickFormat("")
@@ -75,7 +92,7 @@ function make_y_gridlines() {
                 // add the Y gridlines
                 svg.append("g")
                         .attr("class", "grid")
-                        .attr("transform", "translate("+margin.left+"," + margin.top + ")")
+                        .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
                         .call(make_y_gridlines()
                                 .tickSize(-width)
                                 .tickFormat("")
@@ -83,5 +100,62 @@ function make_y_gridlines() {
 
         });
 
+        var refreshChart = function () {
+                d3.json('/telemetrydata', function (err, res) {
+                        if (err) throw err;
+                        alldata.splice(0, 1);
+                        alldata = alldata.concat(res);
+                        res = alldata;
+                        svg.selectAll('g').remove();
+                        g = svg.append('g').attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
+                        var maxTime = d3.max(res, function (d) { return parseDate(d.time); });
+                        var minTime = d3.min(res, function (d) { return parseDate(d.time); });
+                        //var minTime = d3.timeDay.offset(maxTime, -7);
+                        xscale.domain([minTime, maxTime]);
+                        yscale.domain([0, d3.max(res, function (d) { return d.value; })]);
+                        area.y0(yscale(0));
+                        g.append('g')
+                                .attr('transform', 'translate(0,' + height + ')')
+                                .attr('class', 'axiswhite')
+                                .call(d3.axisBottom(xscale));
+                        g.append('g')
+                                .attr('class', 'axiswhite')
+                                .call(d3.axisLeft(yscale));
 
+                        g.append('path')
+                                .datum(res)
+                                .attr('fill', '#00c109')
+                                .attr('class', 'indicator')
+                                .attr('stroke', '#8dff05')
+                                .attr('stroke-width', '2')
+                                .attr('shape-rendering', 'crispEdges')
+                                .attr('style', 'opacity:0.5')
+                                .attr('d', area);
+
+                        // g.append('path')
+                        //         .datum(res)
+                        //         .attr('d',line);
+                        svg.append("g")
+                                .attr("class", "grid")
+                                .attr("transform", "translate(" + margin.left + "," + (height + margin.top) + ")")
+                                .call(make_x_gridlines()
+                                        .tickSize(-height)
+                                        .tickFormat("")
+                                )
+
+                        // add the Y gridlines
+                        svg.append("g")
+                                .attr("class", "grid")
+                                .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+                                .call(make_y_gridlines()
+                                        .tickSize(-width)
+                                        .tickFormat(""))
+                });
+                if(timeserieschart.options.refresh)
+                window.setTimeout(refreshChart, timeserieschart.options.refreshRate);
+        };
+
+        refreshChart();
+
+        return timeserieschart;
 };
